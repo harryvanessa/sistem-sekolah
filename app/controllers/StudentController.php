a2db4bf0849085dd019498a69c7f950c38fde8dc
harryvanessa <harvy.zeus36@gmail.com>
2026-01-27 13:14:06 +0700

    wip: implement student controller

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,21 @@
+<?php
+    namespace App\Controllers;
+
+    class StudentController
+    {
+
+    public function index(): void
+    {
+        echo '<h1>Daftar siswa</h1>';
+        echo '<p>Menampilkan daftar siswa</p>';
+    }
+
+    public function create(): void
+    {
+        echo '<h1>Daftar siswa</h1>';
+        echo '<p>Menampilkan form tambah siswa</p>';
+    }
+
+    }
+
+?>
