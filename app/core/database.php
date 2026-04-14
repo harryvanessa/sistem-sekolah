7fb31379ca5b744d47df67e2884aa2e4b58c05bd
harryvanessa <harvy.zeus36@gmail.com>
2026-04-14 12:30:14 +0700

    feat; implement databse layer

---
+++ app/core/database.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Core;
+
+
+require_once '../app/config/app.php';
+
+class database
+{
+
+    protected $connection;
+
+    public function __construct()
+    {
+        $this->connection = mysqli_connect(
+            DB_HOST,
+            DB_USER,
+            DB_PASSWORD,
+            DB_NAME,
+        );
+
+        if (!$this->connection) {
+            die('Error to connect database');
+        }
+    }
+}
